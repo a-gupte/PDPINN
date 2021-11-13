144ab9212b8540da74e1e96d844b7c318c162598
Aparna Gupte <agupte@mit.edu>
2021-11-12 23:34:58 -0500

    Test adjoint with dot product test

--- test_adjoint.py
+++ test_adjoint.py
@@ -7,6 +7,7 @@ from pyshtools.expand import MakeGridDH
 # from pyshtools.expand import SHExpandDH
 from my_ducc0_wrapper import SHExpandDH
 from pyshtools.spectralanalysis import spectrum
+from my_backends.ducc0_wrapper import MakeGridDH_adjoint_analysis
 
 clm = np.array(
     [[[0, 0, 0],
@@ -21,6 +22,7 @@ clm = np.array(
 print(clm.shape)
 
 function_grid = MakeGridDH(clm, sampling=2)
+print(function_grid.shape)
 # fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 # ax.imshow(function_grid, extent=(0, 360, -90, 90), cmap='viridis')
 # ax.set(xlabel='longitude', ylabel='latitude')
@@ -29,8 +31,9 @@ function_grid = MakeGridDH(clm, sampling=2)
 clm_infer = SHExpandDH(function_grid, sampling=2)
 print(clm_infer)
 
-adjoint = SHExpandDH(clm_infer, sampling=2, flag=True)
-print(adjoint)
+adjoint = MakeGridDH_adjoint_analysis(clm_infer, sampling=2)
+# adjoint = SHExpandDH(clm_infer, sampling=2, flag=True)
+print(adjoint.shape)
 
 # nl = coeffs.shape[1]
 # ls = np.arange(nl)
@@ -47,6 +50,28 @@ print(adjoint)
 # # ax.set_xscale('log')
 # ax.grid()
 # plt.show()
+
+import numpy as np
+
+# x is a 2D map
+# y is spherical harmonic coefficients
+
+# note that this is probably not right because we want some entries of y to be 0
+N = 3
+x = np.random.uniform(0, 1, (2*N, 4*N))
+# y = np.random.uniform(0, 1, (2, N, N))
+y = clm
+
+lhs = SHExpandDH(x, sampling=2)
+lhs = np.sum(y*lhs)
+
+rhs = MakeGridDH_adjoint_analysis(y, sampling=2)
+rhs = np.sum(rhs*x)
+
+print(rhs, lhs, lhs/rhs)
+# the dot product test for the adjoint is not working
+    # could this be a normalization issue? 
+    # then the ratio of lhs and rhs should be the same over different randomizations, which it is!
 
 # power_per_l = spectrum(coeffs)
 # print(power_per_l)
